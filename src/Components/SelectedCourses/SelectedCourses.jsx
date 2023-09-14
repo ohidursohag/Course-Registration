eb9fe97edd58a52ca89afdc20d2934304b1ffa6d
ohidursohag <ohidursohag9595@gmail.com>
2023-09-14 23:12:14 +0600

    Display Selected coursees section layout update

--- src/Components/SelectedCourses/SelectedCourses.jsx
+++ src/Components/SelectedCourses/SelectedCourses.jsx
@@ -3,8 +3,19 @@ import PropTypes from 'prop-types';
 
 const SelectedCourses = props => {
    return (
-      <div>
-         
+      <div className='w-[350px] px-6 bg-white rounded-xl h-max'>
+         <h4 className='text-lg font-bold text-[#2F80ED] border-b-2 py-4'>Credit Hour Remaining 7 hr</h4>
+         <div className='border-b-2 py-4'>
+            <h2 className='text-xl font-bold mb-5'>Course Name</h2>
+            <div>
+               <p className='text-[#1C1B1B99]'>1 Software Engineering</p>
+               <p className='text-[#1C1B1B99]'>1 Software Engineering</p>
+               <p className='text-[#1C1B1B99]'>1 Software Engineering</p>
+               
+            </div>
+         </div>
+            <h3 className='border-b-2 py-4 font-semibold'>Total Credit Hour : 13</h3>
+            <h3 className=' py-4 font-semibold'>Total Price : 48000 USD</h3>
       </div>
    );
 };
